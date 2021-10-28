7469b43fd6e9a273a8bf7c88729c25c1cd3fe2d1
Chris Diehl <cultclassik@gmail.com>
2021-10-28 14:26:00 -0500

    fix local name

--- examples/build/locals.tf
+++ examples/build/locals.tf
@@ -5,5 +5,5 @@ locals {
     created_by = "terratest"
     location   = "westus"
   }
-  vm_name = "${local.tags.created_by}-${var.gh_run_id}-${var.gh_repo}"
+  vm_name = replace("${local.tags.created_by}-${var.gh_run_id}-${var.gh_repo}", "/", "-")
 }
