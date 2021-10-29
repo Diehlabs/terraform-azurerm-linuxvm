379c6483acc80b94c1f40c50f1be22f5776974c4
Chris Diehl <cultclassik@gmail.com>
2021-10-29 09:04:43 -0500

    add https rule to config

--- tests/module_test.go
+++ tests/module_test.go
@@ -30,7 +30,7 @@ func TestMyModule(t *testing.T) {
 
 	terraform.InitAndApply(t, terraformOptions)
 
-	terraform.ApplyAndIdempotent(t, terraformOptions)
+	// terraform.ApplyAndIdempotent(t, terraformOptions)
 
 	resourceGroupName := terraform.Output(t, terraformOptions, "resource_group_name")
 	nsgName := terraform.Output(t, terraformOptions, "nsg_name")
@@ -42,25 +42,15 @@ func TestMyModule(t *testing.T) {
 	assert.NoError(t, err)
 	assert.Equal(t, 8, len(rules.SummarizedRules))
 
+	// ssh rule is contained in the module
+	sshRule := rules.FindRuleByName("SSH")
+	assert.True(t, sshRule.AllowsDestinationPort(t, "22"))
+	assert.False(t, sshRule.AllowsDestinationPort(t, "80"))
+	assert.True(t, sshRule.AllowsSourcePort(t, "*"))
+
+	// https rule is passed to the module
+	httpsRule := rules.FindRuleByName("HTTPS")
+	assert.True(t, httpsRule.AllowsDestinationPort(t, "443"))
+	assert.False(t, httpsRule.AllowsDestinationPort(t, "80"))
+	assert.True(t, httpsRule.AllowsSourcePort(t, "*"))
 }
-
-// func TestTerraformAzureNsgExample(t *testing.T) {
-
-// 	// We should have a rule for allowing ssh
-// 	sshRule := rules.FindRuleByName(sshRuleName)
-
-// 	// That rule should allow port 22 inbound
-// 	assert.True(t, sshRule.AllowsDestinationPort(t, "22"))
-
-// 	// But should not allow 80 inbound
-// 	assert.False(t, sshRule.AllowsDestinationPort(t, "80"))
-
-// 	// SSh is allowed from any port
-// 	assert.True(t, sshRule.AllowsSourcePort(t, "*"))
-
-// 	// We should have a rule for blocking HTTP
-// 	httpRule := rules.FindRuleByName(httpRuleName)
-
-// 	// This rule should BLOCK port 80 inbound
-// 	assert.False(t, httpRule.AllowsDestinationPort(t, "80"))
-// }
